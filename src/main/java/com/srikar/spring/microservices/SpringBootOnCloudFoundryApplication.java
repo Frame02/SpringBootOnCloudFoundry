ee4dbc4e0db3a1bfb23d8ddca226d8eac617d7fb
Srikar Vedantam <srikar.vedantam@hpe.com>
2018-02-11 13:53:35 +0530

    Initial commit...

---
+++ src/main/java/com/srikar/spring/microservices/SpringBootOnCloudFoundryApplication.java
@@ -0,0 +1,12 @@
+package com.srikar.spring.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnCloudFoundryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOnCloudFoundryApplication.class, args);
+	}
+}
